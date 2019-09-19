d3aa59ad5855654a0e66e5987e9783a4f3359926
Tiago Dakuzaku <tdakuzaku@gmail.com>
2019-09-19 05:44:31 -0300

    Docker, NodeJS + Express

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var app = express();
+var router = express.Router();
+
+const PORT = process.env.SERVER_PORT;
+
+router.get("/",function(req,res){
+  res.send({body: "Hello world"});
+});
+
+app.use("/", router);
+
+app.listen(PORT, function () {
+  console.log('=== Upcoming Movies Server is up! ===');
+})
