f17594d53d6331c90345e02bf7732bb63570b5a8
Tiago Dakuzaku <tdakuzaku@gmail.com>
2019-09-20 02:29:27 -0300

    Refactoring request options

--- services/tmdb.js
+++ services/tmdb.js
@@ -1,15 +1,22 @@
 var request = require("request");
 const config = require("../config/tmdb.json");
 
+var getRequestOptions = function(url, query) {
+  var options = {
+    url: config.api_url + url,
+    qs: {
+      api_key: config.api_key
+    }
+  };
+  options.qs = { ...options.qs, ...query };
+  return options;
+};
+
 module.exports = app => {
   app.get("/upcoming", function(req, response) {
-    var options = {
-      url: config.api_url + config.upcoming_url,
-      qs: {
-        api_key: config.api_key,
-        page: req.query.page
-      }
-    };
+    var options = getRequestOptions(config.upcoming_url, {
+      page: req.query.page
+    });
     request(options, function(err, res, body) {
       response.statusCode = res.statusCode;
       response.json(JSON.parse(body));
@@ -17,12 +24,8 @@ module.exports = app => {
   });
 
   app.get("/movie", function(req, response) {
-    var options = {
-      url: config.api_url + config.movie_url + "/" + req.query.id,
-      qs: {
-        api_key: config.api_key
-      }
-    };
+    var url = config.movie_url + "/" + req.query.id;
+    var options = getRequestOptions(url, {});
     request(options, function(err, res, body) {
       response.statusCode = res.statusCode;
       response.json(JSON.parse(body));
@@ -30,13 +33,9 @@ module.exports = app => {
   });
 
   app.get("/search", function(req, response) {
-    var options = {
-      url: config.api_url + config.search_movie_url,
-      qs: {
-        api_key: config.api_key,
-        query: req.query.query
-      }
-    };
+    var options = getRequestOptions(config.search_movie_url, {
+      query: req.query.query
+    });
     request(options, function(err, res, body) {
       response.statusCode = res.statusCode;
       response.json(JSON.parse(body));
