b27ca50aa735c9e56a61763610d5209bdde000ff
Tiago Dakuzaku <tdakuzaku@gmail.com>
2019-09-20 01:56:41 -0300

    Get movie details by ID

--- services/tmdb.js
+++ services/tmdb.js
@@ -1,19 +1,26 @@
 var request = require("request");
 const config = require("../config/tmdb.json");
 
-var getAuthURL = function(url) {
-  return config.api_url + url + "?api_key=" + config.api_key;
-};
-
-const UPCOMING_URL = getAuthURL(config.upcoming_url);
-
 module.exports = app => {
   app.get("/upcoming", function(req, response) {
     var options = {
-      url: UPCOMING_URL,
-      method: "GET",
+      url: config.api_url + config.upcoming_url,
       qs: {
+        api_key: config.api_key,
         page: req.query.page
+      }
+    };
+    request(options, function(err, res, body) {
+      response.statusCode = res.statusCode;
+      response.json(JSON.parse(body));
+    });
+  });
+
+  app.get("/movie", function(req, response) {
+    var options = {
+      url: config.api_url + config.movie_url + "/" + req.query.id,
+      qs: {
+        api_key: config.api_key
       }
     };
     request(options, function(err, res, body) {
--- test/tmdb.test.js
+++ test/tmdb.test.js
@@ -4,6 +4,7 @@ const app = require("../app.js");
 var expect = require("expect.js");
 
 const UPCOMING_URL = "/upcoming";
+const MOVIE_URL = "/movie";
 
 describe("API Authentication", function() {
   it("given valid key then should return status 200", function(done) {
@@ -21,7 +22,7 @@ describe("GET: first 20 Upcoming Movies", function() {
       .end(function(err, res) {
         expect(err).to.be.null;
         expect(res.body.results).to.have.length(20);
-        done();
+        return done();
       });
   });
 });
@@ -37,8 +38,21 @@ describe("GET: with pagination", function() {
           .query({ page: 2 })
           .end(function(err2, res2) {
             expect(res1.body.results[0].id).to.not.be(res2.body.results[0].id);
-            done();
+            return done();
           });
+      });
+  });
+});
+
+describe("GET: movie details by ID", function() {
+  it("should return movies details from a given ID", function(done) {
+    request(app)
+      .get(MOVIE_URL)
+      .query({ id: 540901 })
+      .end(function(err, res) {
+        expect(res.status).to.be(200);
+        expect(res.body.title).to.not.null;
+        return done();
       });
   });
 });
