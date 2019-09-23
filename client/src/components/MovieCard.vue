24c8b119345e7c9b2b01b021ffe1b5e745049cce
Tiago Dakuzaku <tdakuzaku@gmail.com>
2019-09-23 03:30:46 -0300

    Movie genre list

--- client/src/components/MovieCard.vue
+++ client/src/components/MovieCard.vue
@@ -1,49 +1,58 @@
 <template>
     <div>
         <b-card
-            :title="data.title"
-            :img-src="'https://image.tmdb.org/t/p/w500' + data.backdrop_path"
-            :img-alt="data.title"
-            img-top
-            tag="article"
-            class="mb-3"
+          @click="showDetails(data.id)"
+          :img-src="'https://image.tmdb.org/t/p/w500' + data.backdrop_path"
+          :img-alt="data.title"
+          img-top
         >
-        <b-card-text>
-            <div class="movie-card-details" v-if="showDetails">
-                <p>{{data.overview}}</p>
-                <label>Avg. Vote:</label> {{data.vote_average}}
-                <label>Release date:</label> {{data.release_date}}
-            </div>
-        </b-card-text>
-        <b-button v-if="showDetails" v-on:click="lessDetails()">Less details</b-button>
-        <b-button v-else v-on:click="moreDetails()" variant="primary">More details</b-button>
+        <div v-if="data.id == this.showId" class="movie-card-details">
+          <h2>{{data.title}}</h2>
+          <p>{{data.overview}}</p>
+          <label>Genre(s):</label> {{showMovieGenres(data.genre_ids)}}
+          <label>Release date:</label> {{data.release_date}}
+          <label>Avg. Vote:</label> {{data.vote_average}}
+        </div>
     </b-card>
     </div>
 </template>
 
 <script>
-
-// genres: [{"id":28,"name":"Action"},{"id":12,"name":"Adventure"},{"id":16,"name":"Animation"},{"id":35,"name":"Comedy"},{"id":80,"name":"Crime"},{"id":99,"name":"Documentary"},{"id":18,"name":"Drama"},{"id":10751,"name":"Family"},{"id":14,"name":"Fantasy"},{"id":36,"name":"History"},{"id":27,"name":"Horror"},{"id":10402,"name":"Music"},{"id":9648,"name":"Mystery"},{"id":10749,"name":"Romance"},{"id":878,"name":"Science Fiction"},{"id":10770,"name":"TV Movie"},{"id":53,"name":"Thriller"},{"id":10752,"name":"War"},{"id":37,"name":"Western"}],
-
 export default {
   name: 'MovieCard',
-  props: ['data'],
+  props: ['data', 'genres'],
   data () {
     return {
-      showDetails: false
+      showId: null
     }
   },
   methods: {
-    moreDetails () {
-      this.showDetails = true
+    showDetails (id) {
+      this.showId = (this.showId !== id) ? id : null
     },
-    lessDetails () {
-      this.showDetails = false
+    showMovieGenres (ids) {
+      var genreIds = JSON.parse(JSON.stringify(ids))
+      var movieGenres = []
+      this.genres.forEach((el) => {
+        var index = genreIds.indexOf(el.id)
+        if (index >= 0) {
+          movieGenres.push(this.genres[index].name)
+        }
+      })
+      return movieGenres.join(',')
     }
   }
 }
 </script>
 <style>
+  .card-body {
+    color: #FFF;
+    background-color:rgba(0, 0, 0, 0.5);
+    position: absolute;
+    top: 0;
+    height: 100%;
+    padding: 1.25rem 0;
+  }
   .movie-card-details label {
     font-weight: 600;
     margin-left: 10px;
--- client/src/components/Movies.vue
+++ client/src/components/Movies.vue
@@ -18,7 +18,7 @@
     <b-spinner v-if="loading" label="Loading..." class="loading-spinner"></b-spinner>
     <div v-else>
       <b-card-group columns>
-        <movie-card v-for="item in list" v-bind:key="item.id" :data="item"/>
+        <movie-card v-for="item in list" v-bind:key="item.id" :data="item" :genres="genres"/>
       </b-card-group>
       <div>
         <b-button v-on:click="showMore()">Show more</b-button>
@@ -28,6 +28,7 @@
 </template>
 
 <script>
+// TODO create the Navbar compoment
 import MovieCard from '@/components/MovieCard'
 import { RepositoryFactory } from '../repositories/repositoryFactory'
 
@@ -42,6 +43,7 @@ export default {
     return {
       loading: true,
       list: [],
+      genres: [],
       searchText: '',
       currentPage: 1
     }
@@ -53,11 +55,11 @@ export default {
     async fetch () {
       const { data } = await MoviesRepository.list(this.currentPage)
       this.list = this.list.concat(data.results)
+      this.genres = MoviesRepository.genres()
       this.loading = false
     },
     async showMore () {
       this.currentPage++
-      this.loading = true
       this.fetch()
     },
     async search () {
