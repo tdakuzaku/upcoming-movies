6162113f4ea78f6e92a93da0e3a7860e390f0120
Tiago Dakuzaku <tdakuzaku@gmail.com>
2019-09-23 03:53:18 -0300

    Fix heroku server url

--- client/src/repositories/repository.js
+++ client/src/repositories/repository.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const SERVER_URL = 'http://localhost:8080'
+// const SERVER_URL = 'http://localhost:8080'
+const SERVER_URL = 'https://still-coast-13920.herokuapp.com'
 
 export default axios.create({
   baseURL: SERVER_URL,
